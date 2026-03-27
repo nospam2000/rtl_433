43e312eedaab56e37c03949eb0fd416225e654c3
Michael Dreher <Michael.Dreher42@gmail.com>
2026-03-27 12:14:47 +0100

    Fix: heidemann_hx_extension field decoding and documentation

--- src/devices/heidemann_hx_extension.c
+++ src/devices/heidemann_hx_extension.c
@@ -17,21 +17,22 @@ Decoder for Heidemann HX Extension. The original manufacturer is probably Quhwa.
 Original code taken from quhwa.c
 
 The device uses OOK modulation with Pulse Width Coding (PWM):
-- Short pulse (332 us) preceeded by a long gap (479 us) encodes a '0' bit.
-- Long pulse (676 us) preceeded by a short gap (135 us) encodes a '1' bit.
+- Short pulse (332 us) preceeded by a long gap (479 us) encodes a '1' bit.
+- Long pulse (676 us) preceeded by a short gap (135 us) encodes a '0' bit.
 
 The frame length is 13 bits, it is repeated 97 times with a gap of 6200 us between the repeats.
 
 The following information could be part of the message, but is not decoded yet and only a subset can be
 contained because of the short message length:
-- System-ID / House code (8–16 bits): This is the part that forms the "256 radio channels".
-  It is randomly generated or permanently assigned during pairing so that neighbors don't interfere with each other.
-- Command / Melody (4 bits): Often transmits the information about which of the 8 or more melodies the receiver should play.
-- Checksum (optional): Simple check for error detection.
-- Battery status (optional): Some devices might include a bit indicating low battery.
+- Battery status (bit 0): 1=battery ok, 0=low battery; this is not yet confirmed
+- Device-ID (bit 1 to 8): It is randomly generated on each power on
+- Melody (bit 9 to 12): which of the 16 melodies the receiver should play.
 
 Decoding can be tested using these parameters:
     rtl_433 -R 0 -X "n=myproto,m=OOK_PWM,s=332,l=676,g=811,r=6200,bits=13" -Y autolevel
+
+Or the full configuration with field extraction:
+    rtl_433 -R 0 -X "name=Heidemann-HX-Extension-Cfg,modulation=OOK_PWM,s=332,l=676,g=900,r=7000,unique,repeats>=20,bits=13,get=battery_ok:@0:{1},get=id:@1:{8}:%d,get=melody:@9:{4}:%d" -Y autolevel
 
 Check here for some example captures: https://github.com/merbanan/rtl_433_tests/tree/master/tests/heidemann_hx_extension/01
  */
@@ -50,25 +51,20 @@ static int heidemann_hx_extension_decode(r_device *decoder, bitbuffer_t *bitbuff
         return DECODE_FAIL_SANITY;
     }
 
-    bitbuffer_invert(bitbuffer);
-
-    if (bitbuffer->bits_per_row[r] != 13
-            // || (b[1] & 0x03) != 0x03
-            // || (b[2] & 0xC0) != 0xC0
-        )
+    if (bitbuffer->bits_per_row[r] != 13)
         return DECODE_ABORT_LENGTH;
 
     uint32_t rawval = (b[0] << 5) | ((b[1] >> 3) & 0x1F); // 13 bits
     uint32_t melody = (rawval & 0x0F); // 4 bits, there are 8 melodies available
-    uint32_t id = ((rawval >> 4) & 0xFF); // 8 bits and bit 7 is always 0
-    uint32_t batt_low = ((rawval >> 12) & 0x01); // 1 bit ???
+    uint32_t id = ((rawval >> 4) & 0xFF); // 8 bits
+    uint32_t batt_ok = ((rawval >> 12) & 0x01); // 1 bit
 
     /* clang-format off */
     data_t *data = data_make(
             "model",  "",    DATA_STRING, "Heidemann-HX-Extension",
             "id",     "ID",  DATA_INT, id,
             "melody",     "Melody",  DATA_INT, melody,
-            "battery_ok",       "Battery",      DATA_INT,    !batt_low,
+            "battery_ok",       "Battery",      DATA_INT,    batt_ok,
             NULL);
     /* clang-format on */
 
